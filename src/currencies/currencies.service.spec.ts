d634cc2b3a95b6c415d6db17ba23f3df37953337
brianbacca <brianbn05@gmail.com>
2022-03-16 17:37:45 -0300

    test: updateCurrency and  deleteCurrency

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -19,6 +19,8 @@ describe('CurrenciesService', () => {
           useFactory: () => ({
             getCurrency: jest.fn(),
             createCurrency: jest.fn(),
+            updateCurrency: jest.fn(),
+            deleteCurrency: jest.fn(),
           }),
         },
       ],
@@ -79,6 +81,54 @@ describe('CurrenciesService', () => {
     it('should be return when repository return', async () => {
       (repository.createCurrency as jest.Mock).mockReturnValue(mockData);
       expect(await service.createCurrency(mockData)).toEqual(mockData);
+    });
+  });
+
+  describe('updateCurrency()', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.updateCurrency as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException(),
+      );
+      mockData.currency = 'INVALID';
+      await expect(service.updateCurrency(mockData)).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+    it('should be not throw if repository returns', async () => {
+      await expect(service.updateCurrency(mockData)).resolves.not.toThrow();
+    });
+    it('should be called repository with correct params', async () => {
+      await service.updateCurrency(mockData);
+      expect(repository.updateCurrency).toBeCalledWith(mockData);
+    });
+    it('should be throw if value <= 0', async () => {
+      mockData.value = 0;
+      await expect(service.updateCurrency(mockData)).rejects.toThrow(
+        new BadRequestException('The value must be greater zero.'),
+      );
+    });
+    it('should be return when repository return', async () => {
+      (repository.updateCurrency as jest.Mock).mockReturnValue(mockData);
+      expect(await service.updateCurrency(mockData)).toEqual(mockData);
+    });
+  });
+  describe('deleteCurrency()', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.deleteCurrency as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException(),
+      );
+      await expect(service.deleteCurrency('INVALID')).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+
+    it('should be not throw if repository returns', async () => {
+      await expect(service.deleteCurrency('USD')).resolves.not.toThrow();
+    });
+
+    it('should be called repository with correct params', async () => {
+      await service.deleteCurrency('USD');
+      expect(repository.deleteCurrency).toBeCalledWith('USD');
     });
   });
 });
--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,16 +1,19 @@
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 export class Currencies {
   currency: string;
+  value: number;
 }
 export class CurrenciesRepository {
   async getCurrency(currency: string): Promise<any> {
     return new Currencies();
   }
   async createCurrency({ currency, value }): Promise<any> {
+    return new Currencies();
+  }
+  async updateCurrency({ currency, value }): Promise<any> {
+    return new Currencies();
+  }
+  async deleteCurrency(currency: string): Promise<any> {
     return new Currencies();
   }
 }
@@ -22,10 +25,19 @@ export class CurrenciesService {
     return await this.currenciesRepository.getCurrency(currency);
   }
 
-  async createCurrency({ currency, value }): Promise<any> {
+  async createCurrency({ currency, value }): Promise<Currencies> {
     if (value <= 0) {
       throw new BadRequestException('The value must be greater zero.');
     }
     return await this.currenciesRepository.createCurrency({ currency, value });
+  }
+  async updateCurrency({ currency, value }): Promise<Currencies> {
+    if (value <= 0) {
+      throw new BadRequestException('The value must be greater zero.');
+    }
+    return await this.currenciesRepository.updateCurrency({ currency, value });
+  }
+  async deleteCurrency(currency: string): Promise<void> {
+    return await this.currenciesRepository.deleteCurrency(currency);
   }
 }
