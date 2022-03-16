ddad037aadd3e14fd14eb5328c08dca9a5c43997
brianbacca <brianbn05@gmail.com>
2022-03-16 16:34:47 -0300

    test:createCurrency

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -1,10 +1,14 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CurrenciesRepository, CurrenciesService } from './currencies.service';
 
 describe('CurrenciesService', () => {
   let service: CurrenciesService;
   let repository: CurrenciesRepository;
+  let mockData;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,13 +16,17 @@ describe('CurrenciesService', () => {
         CurrenciesService,
         {
           provide: CurrenciesRepository,
-          useFactory: () => ({ getCurrency: jest.fn() }),
+          useFactory: () => ({
+            getCurrency: jest.fn(),
+            createCurrency: jest.fn(),
+          }),
         },
       ],
     }).compile();
 
     service = module.get<CurrenciesService>(CurrenciesService);
     repository = module.get<CurrenciesRepository>(CurrenciesRepository);
+    mockData = { currency: 'USD', value: 1 };
   });
 
   it('should be defined', () => {
@@ -35,6 +43,42 @@ describe('CurrenciesService', () => {
     });
     it('should be not throw if repository returns', async () => {
       await expect(service.getCurrency('USD')).resolves.not.toThrow();
+    });
+    it('should be called repository with correct params', async () => {
+      await service.getCurrency('USD');
+      expect(repository.getCurrency).toBeCalledWith('USD');
+    });
+    it('should be return when repository return', async () => {
+      (repository.getCurrency as jest.Mock).mockReturnValue(mockData);
+      expect(await service.getCurrency('USD')).toEqual(mockData);
+    });
+  });
+  describe('createCurrency()', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.createCurrency as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException(),
+      );
+      mockData.currency = 'INVALID';
+      await expect(service.createCurrency(mockData)).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+    it('should be not throw if repository returns', async () => {
+      await expect(service.createCurrency(mockData)).resolves.not.toThrow();
+    });
+    it('should be called repository with correct params', async () => {
+      await service.createCurrency(mockData);
+      expect(repository.createCurrency).toBeCalledWith(mockData);
+    });
+    it('should be throw if value <= 0', async () => {
+      mockData.value = 0;
+      await expect(service.createCurrency(mockData)).rejects.toThrow(
+        new BadRequestException('The value must be greater zero.'),
+      );
+    });
+    it('should be return when repository return', async () => {
+      (repository.createCurrency as jest.Mock).mockReturnValue(mockData);
+      expect(await service.createCurrency(mockData)).toEqual(mockData);
     });
   });
 });
--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,17 +1,31 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+export class Currencies {
+  currency: string;
+}
 export class CurrenciesRepository {
-  async getCurrency(currency: string): Promise<any> {}
+  async getCurrency(currency: string): Promise<any> {
+    return new Currencies();
+  }
+  async createCurrency({ currency, value }): Promise<any> {
+    return new Currencies();
+  }
 }
 
 @Injectable()
 export class CurrenciesService {
   constructor(private currenciesRepository: CurrenciesRepository) {}
   async getCurrency(currency: string): Promise<any> {
-    try {
-      await this.currenciesRepository.getCurrency(currency);
-    } catch (error) {
-      throw new InternalServerErrorException();
+    return await this.currenciesRepository.getCurrency(currency);
+  }
+
+  async createCurrency({ currency, value }): Promise<any> {
+    if (value <= 0) {
+      throw new BadRequestException('The value must be greater zero.');
     }
+    return await this.currenciesRepository.createCurrency({ currency, value });
   }
 }
